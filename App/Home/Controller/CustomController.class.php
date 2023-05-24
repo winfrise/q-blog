fede218932ada960260ae0aba5f9b0bc8bc1faba
winfrise <fanwenxiang326@163.com>
2023-05-24 18:01:16 +0800

    修改路由

--- App/Home/Controller/CustomController.class.php
+++ App/Home/Controller/CustomController.class.php
@@ -24,43 +24,6 @@ class CustomController extends PublicController {
 
         $category = M('type')->find($_REQUEST['t']);
 
-        switch ($category['type']) {
-            case "1":
-                $type = "article";
-                switch ($_REQUEST['t']) {
-                    //自定义指定文章分类的模板。设计扩充使用。
-                    /*
-                    case "ID": //文章分类ID
-                        $tpl = "/News/image";自定义指定模板位置
-                        break;
-                    */
-                    case "5":
-                        $tpl = "/News/image";
-                        $title_h1 = "资质荣誉";
-                        $this->assign('title_h1', $title_h1);
-                        break;
-                    case "13":
-                        $tpl = "/News/case";
-                        $title_h1 = "资质荣誉";
-                        $this->assign('title_h1', $title_h1);
-                        break;
-                    default:
-                        $tpl = "/News/index";
-                        $page_num = $this->config('page_default');
-
-                }
-                break;
-            case "2":
-                $type = "goods";
-                $tpl = "/Product/index";
-                $page_num = $this->config('page_product');
-                break;
-            case "3":
-                $type = "jobs";
-                $tpl = "/Jobs/index";
-                $page_num = $this->config('page_jobs');
-                break;
-        }
         $count = M($type)->where("`pid` in ($category_id)")->count();
         $page = new \Page($count, $page_num);
 
@@ -70,11 +33,7 @@ class CustomController extends PublicController {
         } else {
             $this->assign('exist', true);
         }
-        if ($type == 'goods') {
-            $type = 'product';
-        } elseif ($type == 'article') {
-            $type = 'news';
-        }
+
 
         foreach ($list as $key => $value) {
             if ($_REQUEST['t'] == '5') {
@@ -93,7 +52,7 @@ class CustomController extends PublicController {
         $this->assign('category', $category['name']);
         $this->assign('page', $page->show());
         $this->assign('list', $list);
-        $this->display($tpl);
+        $this->display();
     }
 
     /**
@@ -123,6 +82,6 @@ class CustomController extends PublicController {
 
         $this->assign('article', $article);
         $this->assign('user', $user);
-        $this->display($tpl);
+        $this->display();
     }
 }
